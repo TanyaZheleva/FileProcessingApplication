c08d5f836f62533aa5f34c2262879ffcfe8c44ef
TanyaZheleva <tanya.n.zheleva@gmail.com>
2020-06-18 10:41:53 +0300

    basic structure

---
+++ fpa.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+
+class File:
+    lines = []
+    numbers = []
+
+    def get_lines(self,fd):
+        self.lines=fd.readlines()
+        num=""
+        for j in self.lines:
+            sublist = []
+            for q in j:
+                if q == " ":
+                    sublist.append(int(num))
+                    num=""
+                else:
+                    num+=q
+            self.numbers.append(sublist)
+#        print (numbers)
+
+#    def validate_file(self, fd):
+        
+        
+#    def switch_lines(self, fd, l1, l2):
+#    def switch_numbers(self, fd, l1, n1, l2, n2):
+#    def save_file(self, fd, filename):
+#    def insert_number(self, fd, line, index, number):
+#    def read_number(self, fd, line, index):
+#    def replace_number(self, fd, line, index, number):
+#    def remove_number(self, fd, line, index):
+
+
+filename = input ("Enter the full path to a file you would like to work with: ")
+
+print ("a. validate the file contents\nb.switch two lines by line indexes\nc.switch two numbers by line and number indexes\nd.the result file be saved in the original after validations on the format and the given indexes\ne.1.insert at position\ne.2.read a number at a position\ne.3.modify a number at posigion\ne.4.remove a number at position")
+
+option = input ("Select an option and input arguments: ")
+print (option)
+
+fd = open(filename,'r+')
+
+myobject = File()
+myobject.get_lines(fd)
+myobject.validate_file(fd)
+
+#fd.close()
